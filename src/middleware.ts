f674d9e58768b69e9cb86f662f2466b436c1cf67
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-07-30 13:27:49 +0900

    스태시

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export default async function middleware(req: NextRequest) {
   // 로그인 페이지 유저 정보가 있을때 접근 못하게 함
   if (pathname.startsWith("/auth") && session) {
     const referer = req.headers.get("referer") || "/";
-    console.log("Redirecting to referer:", referer);
+
     return NextResponse.redirect(new URL(referer, req.url));
   }
 }
